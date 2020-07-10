cea11dd3bf54f819c37454d6e3cf9885dc7f32ec
jianxianp <jian_xianpeng@hotmail.com>
2020-07-10 11:48:01 +0800

    提交

---
+++ day0710/src/main/java/com/jxp/Demo.java
@@ -0,0 +1,10 @@
+package com.jxp;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("测试git");
+        System.out.println("测试git");
+        System.out.println("测试git");
+        System.out.println("测试git");
+    }
+}
